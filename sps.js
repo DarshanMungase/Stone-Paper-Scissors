6d1be8789401bb32da79a1812dddb47c14ec02ac
Darshan Mungase <darshan.mungase@walchandsangli.ac.in>
2025-07-24 18:55:52 +0530

    Add files via upload

---
+++ sps.js
@@ -0,0 +1,38 @@
+const choices = document.querySelectorAll(".choice") ;
+const userscore = document.querySelector("#user") ;
+const compscore = document.querySelector("#comp") ;
+const msg = document.querySelector("#msg") ;
+const gaincompchoice = () => {
+    const options = ["stone","paper","scissors"] ;
+    const randIdx = Math.floor(Math.random()*3) ;
+    return options[randIdx] ;
+};
+const playgame = (userchoice) => {
+    const compchoice = gaincompchoice() ;
+    console.log(compchoice);
+    if(compchoice == userchoice) {
+        msg.innerText = "Draw game... Play Again..." ;
+    }
+    else if(compchoice == "stone" && userchoice == "scissors") {
+        msg.innerText = "Bot Win... Play Again" ;
+        compscore.innerText++ ;
+    }
+    else if(compchoice == "paper" && userchoice == "stone") {
+        msg.innerText = "Bot Win... Play Again" ;
+        compscore.innerText++ ;
+    } 
+    else if(compchoice == "scissors" && userchoice == "paper") {
+        msg.innerText = "Bot Win... Play Again" ;
+        compscore.innerText++ ;
+    }
+    else {
+        msg.innerText = "Congratulation!... You Win..." ;
+        userscore.innerText++ ;
+    }
+}
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userchoice = choice.getAttribute("id") ;
+        playgame(userchoice) ;
+    })
+})
